1c2117ebac6da056e3540267707f3a8d9b27d194
Juan Pablo Molina <juanpablomolina@MacBook-Pro-de-juan.local>
2022-11-07 20:32:31 -0300

    proyecto js

---
+++ js/productos.js
@@ -0,0 +1,12 @@
+const productos = [
+    { id: 01, tipo: "Caja", nombre: "Caja N°1", precio: 1050, foto: "../media/boxs/box1.jpeg" },
+    { id: 02, tipo: "Caja", nombre: "Caja N°2", precio: 1000, foto: "../media/boxs/box2.jpeg" },
+    { id: 03, tipo: "Caja", nombre: "Caja N°3", precio: 1050, foto: "../media/boxs/box3.jpeg" },
+    { id: 04, tipo: "Caja", nombre: "Caja N°4", precio: 1000, foto: "../media/boxs/box4.png" },
+    { id: 05, tipo: "Caja", nombre: "Caja N°5", precio: 1400, foto: "../media/boxs/box5.jpeg" },
+    { id: 06, tipo: "Caja", nombre: "Caja N°6", precio: 1500, foto: "../media/boxs/c1.jpeg" },
+    { id: 07, tipo: "Caja", nombre: "Caja N°7", precio: 2500, foto: "../media/boxs/c2.jpeg" },
+    { id: 08, tipo: "Caja", nombre: "Caja N°8", precio: 1800, foto: "../media/boxs/c3.jpeg" },
+    { id: 09, tipo: "Caja", nombre: "Caja N°9", precio: 1800, foto: "../media/boxs/c4.jpeg" },
+    { id: 10, tipo: "Caja", nombre: "Caja N°10", precio: 1800, foto: "../media/boxs/c5.jpeg" },
+];
